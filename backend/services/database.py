820019a36bfe3e7c321df94ac67c8059bce1064e
Divyanshu <lyricalv15@gmail.com>
2025-09-07 16:27:41 +0530

    Backend

--- backend/services/database.py
+++ backend/services/database.py
@@ -10,7 +10,7 @@ def get_db_connection():
 def init_db():
     conn = get_db_connection()
     
-    # Users table
+    # Users table   
     conn.execute('''
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
